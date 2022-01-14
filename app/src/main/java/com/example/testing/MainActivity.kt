b5e40fc408581b8baed56aa99b0070b37a258988
Christiandv <christian.david.vaughan@gmail.com>
2022-01-13 22:35:28 -0500

    Halfway through tutorial

--- app/src/main/java/com/example/testing/MainActivity.kt
+++ app/src/main/java/com/example/testing/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.testing
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
     }
+    /** Called when the user touches the button */
+    fun enterButtonClicked(view: View) {
+        var text:EditText = findViewById(R.id.editText);
+        Log.d("Testing", text.text.toString())
+    }
+
+
+
+
 }
